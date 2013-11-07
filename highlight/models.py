5b6135923f7301c6161134df5199b18720b52589
Kayvon Tehranian <ktehranian@gmail.com>
2013-11-06 18:21:33 -0800

    Adding initial model: Moment

--- highlight/models.py
+++ highlight/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Moment(models.Model):
+  text = models.CharField(max_length=140)
+  timestamp = models.DateTimeField(auto_now_add=True)
+
+  def __unicode__(self):
+    return self.text
